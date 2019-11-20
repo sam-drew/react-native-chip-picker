70012f780f1edd21c9ed6507179142508d2a6659
Sam Drew <sam-drew@outlook.com>
2019-11-20 20:29:18 +0000

    + Support for custom selected chip colour.

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ import {
 
 export default class RNChipPicker extends React.Component {
     render() {
+        // Check & set the colour for selected chips.
+        var selectedColour = '#007FFF';
+        if (this.props.selectedColour != undefined) {
+            selectedColour = this.props.selectedColour
+        };
+
         const style = StyleSheet.create({
             selected: {
-                paddingTop: 10,
-                paddingBottom: 10,
-                backgroundColor: '#007FFF',
-                alignItems: 'center',
-                justifyContent: 'center',
-                flex: (1/this.props.items.length),
+                backgroundColor: selectedColour,
             },
             notSelected: {
                 paddingTop: 10,
@@ -49,15 +50,17 @@ export default class RNChipPicker extends React.Component {
         for (i=0; i<(this.props.items.length); i++) {
             var chipStyle = [style.notSelected];
             const item = this.props.items[i];
+            // Check if the chip being rendered has been selected.
             if (item == this.props.value) {
-                chipStyle = [style.selected];
+                chipStyle.push(style.selected);
             };
+            // Check if the chip is the first or last in the list, if it is, make sure
+            // that the correct corners are rounded.
             if (i == 0) {
                 chipStyle.push(style.firstItem);
             } else if (i == (this.props.items.length - 1)) {
                 chipStyle.push(style.lastItem);
             };
-            var testText = 'Text';
             chips.push(
                 <TouchableHighlight key={i} style={chipStyle} onPress={() => this.props.onValueSelect(item)}>
                     <View>
